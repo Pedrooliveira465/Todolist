108d8602efc4c1c00b43135319f706f1f61485c0
Pedro Oliveira <pedro86gugalegal@gmail.com>
2023-10-15 08:46:41 -0300

    Created FindUserById

--- src/main/java/br/com/pedrooliveira/todolist/task/TaskController.java
+++ src/main/java/br/com/pedrooliveira/todolist/task/TaskController.java
@@ -1,12 +1,13 @@
 package br.com.pedrooliveira.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,8 +41,11 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
-    public void list() {
-        
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
     }
 
 }
